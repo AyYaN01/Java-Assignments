d4941bf72e0a9fc5feb78b688e42143ea7b95ab7
AyYaN <88164465+AyYaN01@users.noreply.github.com>
2022-06-11 22:12:30 +0500

    Add files via upload

---
+++ PassByValue.java
@@ -0,0 +1,21 @@
+public class PassByValue
+{
+    public static void main(String[] args) 
+    {
+        int a=42, b=59;
+        PBvalue obj=new PBvalue();
+        System.out.println("Before Swap \nA= "+a+"\n B= "+b);
+        obj.swap(a, b);
+        System.out.println("Before Swap \nA= "+a+"\n B= "+b);
+    }
+}
+class PBvalue
+{
+    void swap(int x, int y)
+    {
+        int temp;
+        temp=x;
+        x=y;
+        y=temp;
+    }
+}
